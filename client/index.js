f78af149915b00c4cbf3a6fcbe5d39f8b0c7e70f
PRO <hirangpatel2@gmail.com>
2021-04-11 18:48:50 +0530

    fixed issue with giveways

--- client/index.js
+++ client/index.js
@@ -3,6 +3,7 @@ const { GiveawayClient } = require('reconlx')
 const config = require('../config.json')
 
 const giveaway = new GiveawayClient({
-   defaultColor: "00FFF9"
+   defaultColor: "00FFF9",
+   mongoURI: "mongodb+srv://last:last@test.8ukwy.mongodb.net/Data"
 })
 module.exports = giveaway;

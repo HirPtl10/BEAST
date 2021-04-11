a8194a4678e3ccc228ea75b54c08b0c04a9505b8
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-04-11 15:35:00 +0530

    Update kala.js

--- commands/admin/kala.js
+++ commands/admin/kala.js
@@ -6,7 +6,7 @@ module.exports = {
     
     run : async(client, message, args) => {
         if(message.author.id !== '827793921144913971') return message.channel.send('This is an owner only command.')
-        const User = message.guild.members.cache.get(args[0])
+        const User = message.mentions.users.first();
         if(!User) return message.channel.send('User is not valid.')
 
         blacklist.findOne({ id : User.user.id }, async(err, data) => {

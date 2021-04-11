8254313b6e9d9e8fe87659a97105576fa2af7950
PRO <hirangpatel2@gmail.com>
2021-04-11 19:57:57 +0530

    a

--- models/autorole.js
+++ models/autorole.js
@@ -4,4 +4,4 @@ const Schema = new mongoose.Schema({
     guildId: String,
     roleId: String
 })
-mongoose.model = module.exports('autojoin', Schema)
+module.exports = mongoose.model('autorole', Schema)

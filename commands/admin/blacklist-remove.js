a70558c70ff7704180c0a567fe7d525541f0c34c
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-04-11 15:45:06 +0530

    Create blacklist-remove.js

---
+++ commands/admin/blacklist-remove.js
@@ -0,0 +1,23 @@
+const blacklist = require('../../models/blacklist')
+const { Message } = require('discord.js')
+
+module.exports = {
+    name : 'blacklist-remove',
+    run : async(client, message, args) => {
+        if(message.author.id !== '827793921144913971') return message.channel.send('This is an owner only command.')
+        const User = message.mentions.members.first();
+        if(!User) return message.channel.send('User is not valid.')
+
+        blacklist.findOne({ id : User.user.id }, async(err, data) => {
+            if(err) throw err;
+            if(data) {
+               await blacklist.findOneAndDelete({ id : User.user.id })
+                .catch(err => console.log(err))
+                message.channel.send(`${User} has been removed from blacklist.`)
+            } else {
+               message.channel.send(`${User} is not blacklisted.`)
+            }
+           
+        })
+    }
+}

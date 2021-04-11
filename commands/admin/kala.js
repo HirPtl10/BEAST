8312c9ba85009e077b80c5ae9eaf1b01a3f4542a
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-04-11 15:19:19 +0530

    Update kala.js

--- commands/admin/kala.js
+++ commands/admin/kala.js
@@ -1,1 +1,25 @@
+const blacklist = require('../../models/blacklist')
+const { Message } = require('discord.js')
 
+module.exports = {
+    name : 'kala',
+    
+    run : async(client, message, args) => {
+        if(message.author.id !== '827793921144913971') return message.channel.send('This is an owner only command.')
+        const User = message.guild.members.cache.get(args[0])
+        if(!User) return message.channel.send('User is not valid.')
+
+        blacklist.findOne({ id : User.user.id }, async(err, data) => {
+            if(err) throw err;
+            if(data) {
+                message.channel.send(`**${User.displayName}** has already been blacklisted!`)
+            } else {
+                data = new blacklist({ id : User.user.id })
+                data.save()
+                .catch(err => console.log(err))
+            message.channel.send(`${User.user.tag} has been added to blacklist.`)
+            }
+           
+        })
+    }
+}

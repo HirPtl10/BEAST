45b1f463f9e9a266246f915b4ddad24d26c596f2
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-04-12 17:12:19 +0530

    Update dm.js

--- commands/fun/dm.js
+++ commands/fun/dm.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'dm',
   run: async (client, message, args) => {
     if (!message.member.hasPermission("ADMINISTRATOR") return;
-        const target = message.mentions.members.first();
+     
       const words = args.slice(1).join(' ')
       message.mentions.members.first.send(words)
   }

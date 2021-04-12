a23a7e27201d36830463eba8f758ed2bd4c007aa
PRO <hirangpatel2@gmail.com>
2021-04-12 19:20:58 +0530

    a

--- commands/fun/dm.js
+++ commands/fun/dm.js
@@ -2,8 +2,10 @@ module.exports = {
   name: 'dm',
   run: async (client, message, args) => {
     if (!message.member.hasPermission("ADMINISTRATOR")) return;
-      const target = message.mentions.members.first();
-      const words = args.slice(1).join(' ')
-      target.send(words)
+    const target = args[0]
+    const words = args.slice(1).join(' ')
+    client.users.fetch(target).then(async user => {
+    target.send(words)
+    })
   }
 }

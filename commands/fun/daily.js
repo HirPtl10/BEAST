93bbc1b14dcb78cea230100a5373be18297654d7
PRO <hirangpatel2@gmail.com>
2021-04-13 20:58:24 +0530

    a

--- commands/fun/daily.js
+++ commands/fun/daily.js
@@ -1,33 +1,15 @@
-const db = require('quick.db');
-const ms = require('parse-ms');
-require("../../ExtendedMessage");
-
+const { Client, Message, MessageEmbed, Discord } = require('discord.js');
+const mongoose = require('mongoose')
+const schema = require('../../models/schema')
 module.exports = {
-    name: "daily",
-    description: "Receive a daily award of money",
-
-    async run (client, message, args) {
-        let user = message.author;
-        let timeout = 86400000;
-        let amount = 500;
-
-        let daily = await db.fetch(`daily_${message.guild.id}_${user.id}`);
-
-        if(daily !== null && timeout - (Date.now() - daily) > 0){
-            let time = ms(timeout - (Date.now() - daily));
-            const embed = new Discord.MessageEmbed();
-            embed.setTitle(`Daily Income Alredy Collected`)
-            embed.setColor(`RED`)
-            embed.setDescription(`You have already collected your daily coins \n Come back in ${time.days}d, ${time.hours}h, ${time.minutes}m, and ${time.seconds}s`)
-            message.channel.send(embed);	
-             } else {
-            db.add(`money_${message.guild.id}_${user.id}`, amount);
-            db.set(`daily_${message.guild.id}_${user.id}`, Date.now());
-            const embed = new Discord.MessageEmbed();
-            embed.setTitle(`Daily Income`);
-            embed.setDescription(`Successfully added ${amount} coins to your account`)
-            embed.setColor(`Green`)
-        message.channel.send(embed);	
-             }
-    }
+   name: '',
+   description: '',
+   aliases: '',
+   usage: '',
+   timeout: '',
+   cooldown: '',
+   run: async (client, message, args) => {
+    client.add(message.author, parseInt('2000'))
+    message.channel.send(`Done`)
+   }
 }

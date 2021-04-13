592b571e4fc4cb3256c2877842c5e5cc950c3ed7
PRO <hirangpatel2@gmail.com>
2021-04-13 18:35:37 +0530

    a

--- commands/fun/add.js
+++ commands/fun/add.js
@@ -10,7 +10,7 @@ module.exports = {
    run: async (client, message, args) => {
     const target = message.mentions.members.first()
         if (!target) message.reply(`Please mention a person`)
-        client.add(target.id, parseInt(args[0]));
+        client.add(target.id, parseInt(args[1]));
         message.channel.send(`Added money on ${tagret} balance`)
    }
 }

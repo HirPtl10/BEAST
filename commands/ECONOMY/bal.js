721a6d5b19132060e7fcb5765377a9037d5e2bb4
PRO <hirangpatel2@gmail.com>
2021-04-13 21:58:41 +0530

    a

--- commands/ECONOMY/bal.js
+++ commands/ECONOMY/bal.js
@@ -12,7 +12,7 @@ module.exports = {
        const bal = await client.bal(target.id)
        const embed = new MessageEmbed();
        embed.setTitle(`Balance of ${target.username}`)
-       embed.setDescription(`${target} has \` `+ bal + '\` coins🪙')
+       embed.setDescription(`${target} has \``+ bal + '\` coins🪙')
        embed.setColor(`BLUE`)
        message.channel.send(embed);
 

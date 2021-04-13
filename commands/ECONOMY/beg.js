cdcf3c00fd8d929e8a5d08aadc1699dc9688c2d1
PRO <hirangpatel2@gmail.com>
2021-04-13 21:30:54 +0530

    a

---
+++ commands/ECONOMY/beg.js
@@ -0,0 +1,17 @@
+const { Client, Message, MessageEmbed, Discord } = require('discord.js');
+
+module.exports = {
+   name: 'beg',
+   description: '',
+   aliases: '',
+   usage: '',
+   timeout: '',
+   cooldown: '',
+   run: async (client, message, args) => {
+    const randomNumber = Math.floor(Math.random() * 500) + 1;
+    client.add(message.author.id, parseInt(randomNumber));
+    const embed = new MessageEmbed();
+   embed.setTitle(`Lol You begged`)
+   embed.setDescription(`You begged And earned \`${randomNumber}\``)
+   }
+}

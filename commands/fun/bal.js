d7c8a7011db4935dc3adafa40afa1c3573d8e5d7
PRO <hirangpatel2@gmail.com>
2021-04-13 09:53:12 +0530

    a

--- commands/fun/bal.js
+++ commands/fun/bal.js
@@ -17,6 +17,6 @@ module.exports = {
         serverId: message.guild.id,
         coins: 1000
     })
-    message.channel.send(`Balance is ${ProfileModels.coins}`)
+    message.channel.send(`Balance is ${ProfileModels.coins}` || `Balance is 0`)
    }
 }

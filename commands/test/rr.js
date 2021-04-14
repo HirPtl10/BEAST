0954014e6174497bcb13241a56dd3a767059c45d
PRO <hirangpatel2@gmail.com>
2021-04-14 18:00:08 +0530

    s

--- commands/test/rr.js
+++ commands/test/rr.js
@@ -43,7 +43,8 @@ module.exports = {
                         },
                     ],
                 },
-            }).save();
+            });
+            Schema.save()
         }
         message.channel.send(`new role added`)
     });

0458b55abc535f63b1725c768a63a928ae7c886b
PRO <hirangpatel2@gmail.com>
2021-04-14 11:30:24 +0530

    a

--- commands/ECONOMY/buy.js
+++ commands/ECONOMY/buy.js
@@ -1,6 +1,7 @@
 const { Client, Message, MessageEmbed, Discord } = require('discord.js');
 const mongoose = require("mongoose")
 const inventory = require('../../models/inventory')
+const items = require('../../events/shopitems')
 
 module.exports = {
    name: 'buy',
@@ -10,6 +11,42 @@ module.exports = {
    timeout: '',
    cooldown: '',
    run: async (client, message, args) => {
+      if (!args[0]) return message.channel.send('Please provide a item to buy')
+      const itemToBuy = args[0].toLowerCase();
 
-   }
-}
+      const validItem = !!items.find((val)=> val.item.toLowerCase() === itemToBuy);
+      if (!validItem) return message.channel.send('That item is not in shop')
+
+      const itemPrice = items.find((val) => val.item.toLowerCase() === itemToBuy).price;
+
+      const userBalance = await client.bal(message.author.id);
+      if (userBalance < itemPrice) return message.channel.send(`You cant buy this item u only have ${userBalance} coins`)
+      const params = {
+         Guild: message.guild.id,
+         User: message.author,
+         
+      }
+      inventory.findOne(params, async (err, data)=>{
+         if (data){
+            const hasItem = Object.keys(data.Inventory.includes).includes(itemToBuy);
+            if (!hasItem){
+               data.Inventory[itemToBuy] = 1;
+            } else {
+               data.Inventory[itemToBuy]++
+            }
+            console.log(data);
+           await inventory.findOneAndUpdate(params, data)
+         } else {
+            new inventory({
+               Guild: message.guild.id,
+               User: message.author,
+               Inventory: {
+                  [itemToBuy]: 1
+               },
+               
+            }).save()
+         }
+         message.channel.send(`You have bought ${itemToBuy}`)
+      })
+   },
+};

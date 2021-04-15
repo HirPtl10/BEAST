0f004903acbcb139fc86910929c723955227f790
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-04-15 22:44:20 +0530

    Update and rename delete.js to cdelete.js

--- commands/admin/cdelete.js
+++ commands/admin/cdelete.js
@@ -1,7 +1,7 @@
 const schema = require('../../models/custom-commands');
 
 module.exports = {
-    name: 'cc-delete',
+    name: 'cdelete',
     run: async(client, message, args) => {
         if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('You do not have permissions to use this command');
 

3038f51b4f5c7a8362eb6cbff07f714a92d43e70
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-04-15 16:06:28 +0530

    Update inventory.js

--- commands/ECONOMY/inventory.js
+++ commands/ECONOMY/inventory.js
@@ -13,15 +13,14 @@ module.exports = {
    run: async (client, message, args) => {
       inventory.findOne({ Guild: message.guild.id, User: message.author.id}, async(err, data)=>{
         if (!data) return message.channel.send(`${message.author} You have no items in your inventory`)  
-        const embed = new MessageEmbed();
-       embed.setTitle(`${message.author.username} Your Inventory`)
-       embed.setDescription(
-        Object.keys(data.Inventory).map((key)=>
-        `${key} (${data.Inventory[key]})`
-        ),
-       );
-       })
        
+       const mapd = Object.keys(data.Inventory).map((key)=>
+        `${key} (${data.Inventory[key]})`
+        );
+       })
+      const embed = new MessageEmbed()
+      embed.setTitle(`ok`)
+       embed.setDescription(mapd)
         message.channel.send(embed);
         
    },

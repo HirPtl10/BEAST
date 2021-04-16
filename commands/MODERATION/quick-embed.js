9acc87b602b968aacb42d7b4e2d78bb2811fe91e
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-04-16 13:05:33 +0530

    Update quick-embed.js

--- commands/MODERATION/quick-embed.js
+++ commands/MODERATION/quick-embed.js
@@ -1,15 +1,15 @@
 const Discord = require('discord.js);
 module.exports = {
-  // Do this according to your command handler
+ 
   name: "quick-embed",
   aliases: ['make-embed'],
   run: async (client, message, args) => {
-    // Code Begin
+  
     let embed = null;
-    const Tochannel = message.mentions.channels.first(); // Take first channel mention from the message
+    const Tochannel = message.mentions.channels.first(); 
     if (!Tochannel) return message.channel.send('Specify a channel to send embed!')
-    args.shift(); // Shifting argument because args[0] is channel mention!
-    const arg = args.join(" "); // Joining args to split it by '^' Symbol!
+    args.shift(); 
+    const arg = args.join(" ");
     const title = arg.split('^')[0];
     if (!title) return message.channel.send('Specify a title for the embed!')
     const description = arg.split('^')[1];

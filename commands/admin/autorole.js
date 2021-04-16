e575b64ddcbf50d63f6192b21a9d5ba023849323
PRO <hirangpatel2@gmail.com>
2021-04-16 11:15:34 +0530

    a

--- commands/admin/autorole.js
+++ commands/admin/autorole.js
@@ -11,32 +11,14 @@ module.exports = {
    timeout: '',
    cooldown: '',
    run: async (client, message, args) => {
-    const Autorole = message.mentions.roles.first() || message.guild.roles.cache.find(r => r.name.toLowerCase() === args.slice(1).join(' ') || r.name === args[0]) || message.guild.roles.cache.find(r => r.id.toLowerCase()=== args.slice(0).join(" ") || r.id === args[0])
-    await auto.findOne({
-        guildId: message.guild.id
-    }, async (err, data) => {
-        if (err) console.error(err);
-        if (!auto) {
-            const data = new auto({
-              
-               guildId: message.guild.id,
-               roleId: Autorole.id
-            });
+        const Autorole = message.mentions.roles.first();
 
-            await data.save()
-            .then(result => console.log(result))
-            .catch(err => console.error(err));
-
-            return message.channel.send(`Auto join role updated`);
-        } else {
-            auto.updateOne({
-                roleId: Autorole.id
-            })
-            .then(result => console.log(result))
-            .catch(err => console.error(err));
-
-            return message.channel.send(`Auto join role updated`);
-        };
-    });
-    }
+        const data = await auto.findOne({ guildId: message.guild.id })
+        const newData = new auto({
+            guildId: message.guild.id,
+            roleId: Autorole.id,
+        })
+        await newData.save();
+        
+    } 
 }

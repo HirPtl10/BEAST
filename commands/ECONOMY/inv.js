5bc9e316604f428a2f8e46332e5700c6c3c0d51e
PRO <hirangpatel2@gmail.com>
2021-04-16 16:47:54 +0530

    a

--- commands/ECONOMY/inv.js
+++ commands/ECONOMY/inv.js
@@ -9,9 +9,9 @@ module.exports = {
       if (!data) return message.reply('You have nothing')
       const mappedData = Object.keys(data.Inventory)
       .map((key) => {
-        return `**${key}** --- ${data.Inventory[key]}`;
+        return `> **${key}** ➣ ${data.Inventory[key]}`;
       })
-      .join("\n")
+      .join("\n\n")
       const embed = new Discord.MessageEmbed();
      embed.setTitle(`${message.author.username} Inventory`)
       .setDescription(mappedData)

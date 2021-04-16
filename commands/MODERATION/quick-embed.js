e9cdf8ca8088b3ca9c99c8c47ff7ef296c532c5f
PRO <hirangpatel2@gmail.com>
2021-04-16 16:32:02 +0530

    .

--- commands/MODERATION/quick-embed.js
+++ commands/MODERATION/quick-embed.js
@@ -11,18 +11,19 @@ module.exports = {
     if (!Tochannel) return message.channel.send('Specify a channel to send embed!')
     args.shift(); 
     const arg = args.join(" ");
-    const title = arg.split('^')[0];
+    const title = arg.split('|')[0];
     if (!title) return message.channel.send('Specify a title for the embed!')
-    const description = arg.split('^')[1];
+    const description = arg.split('|')[1];
     if (!description) return message.channel.send('Specify a description for the embed!')
-    const footer = arg.split('^')[2];
-    if (!footer) return message.channel.send('Specify a footer for the embed!')
+    const color = arg.split('|')[2];
+    if (!footer) return message.channel.send('Specify a color for the embed!')
 
+ 
  
 const embed = new MessageEmbed();
     embed.setTitle(title)
     embed.setDescription(description)
-    embed.setFooter(footer)
+    embed.setColor(color)
    Tochannel.send(embed)
   }
 }

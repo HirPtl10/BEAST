0e507187d026b51d345a86a8bbf9e029301621d6
PRO <hirangpatel2@gmail.com>
2021-04-16 10:41:40 +0530

    s

--- events/autorole.js
+++ events/autorole.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 const Autorole = require('../commands/admin/autorole')
 const client = require('../index')
-const db = require('../models/autorole')
+const Schema = require('../models/autorole')
 
 client.on('guildMemberAdd', async (member) => {
-    db.findOne({ roleId : Autorole.id }, async(err, data) => {
+    Schema.findOne({ guildId: member.guild.id  ,roleId : Autorole.id }, async(err, data) => {
         if(err) throw err;
         if(!data) {
             return

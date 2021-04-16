eb7527fd4edfd2b9ac550bcf9ccef24f907d457f
PRO <hirangpatel2@gmail.com>
2021-04-16 16:13:38 +0530

    a

--- commands/MODERATION/quick-embed.js
+++ commands/MODERATION/quick-embed.js
@@ -1,1 +1,29 @@
+const { MessageEmbed } = require('discord.js');
+           
+module.exports = {
+ 
+  name: "quick-embed",
+  aliases: ['make-embed'],
+  run: async (client, message, args) => {
+  
+    let embed = null;
+    const Tochannel = message.mentions.channels.first(); 
+    if (!Tochannel) return message.channel.send('Specify a channel to send embed!')
+    args.shift(); 
+    const arg = args.join(" ");
+    const title = arg.split('^')[0];
+    if (!title) return message.channel.send('Specify a title for the embed!')
+    const description = arg.split('^')[1];
+    if (!description) return message.channel.send('Specify a description for the embed!')
+    const footer = arg.split('^')[2];
+    if (!footer) return message.channel.send('Specify a footer for the embed!')
+
+ 
+const embed = new MessageEmbed();
+    embed.setTitle(title)
+    embed.setDescription(description)
+    embed.setFooter(footer)
+   Tochannel.send(embed)
+  }
+}
 

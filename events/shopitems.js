95e881d515f7f6d6951b95b75f3238e0cd99a2f1
PRO <hirangpatel2@gmail.com>
2021-04-16 16:54:50 +0530

    a

--- events/shopitems.js
+++ events/shopitems.js
@@ -1,11 +1,11 @@
 module.exports = [
     {
-        item: '🍌 BANANA',
+        item: 'BANANA',
         price: '1000',
         description: 'This is banana which will help in beg of coins'
     },
     {
-        item: '<:dildo:832576178224889857> DILDO',
+        item: 'DILDO',
         price: '2000',
         description: 'Just of Show Just have fun using'
     },

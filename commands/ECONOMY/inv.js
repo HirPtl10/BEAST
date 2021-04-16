38b66f89ee4f0f378691b4f342adce4b4408d9c6
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-04-16 16:03:10 +0530

    Create inv.js

---
+++ commands/ECONOMY/inv.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const inventory = require('../../models/inventory')
+
+module.exports = {
+  name: 'inv',
+  run: async(client, message, args) => {
+    inventory.findOne({ Guild: message.guild.id, User: message.author.id }, async(err, data) => {
+      if (!data) return message.reply('You have nothing')
+      const mappedData = Object.keys(data.Inventory)
+      .map((key) => {
+        return `${key}(${data.Inventory[key]})`;
+      })
+      .join("\n\n")
+      const embed = new Discord.MessageEmbed();
+      .setTitle(`${message.author.username} Inventory`)
+      .setDescription(mappedData)
+      message.channel.send(embed)
+    }
+   );
+  },
+};

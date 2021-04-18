6881b41ce7018f215c05aafd1e233d3ab739ad92
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-04-18 22:29:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -164,7 +164,25 @@ const data = await schema.findOne({ Guild: message.guild.id, Command: name })
 
 client.login(process.env.token)
 
+client.on('messageDelete', async(message) => {
+    require('./Logging/MessageDelete')(message)
+})
+client.on('messageUpdate', async(oldMessage, newMessage) => {
+    require('./Logging/MessageEdit')(oldMessage, newMessage)
+})
+client.on('roleCreate', async (role) => {
+require('./Logging/RoleCreate')(role)
+})
+client.on('memberRoleUpdate', async (member, role)=>{
+require('./Logging/memberUpdate')(member, role)
+})
 
+client.on('guildBanAdd', async (user, guild)=>{
+require('./Logging/Ban')(user, guild)
+})
+client.on('guildMemberAdd', async (member)=>{
+    require('./Logging/guildMemberAdd')(member)
+})
 
 
 

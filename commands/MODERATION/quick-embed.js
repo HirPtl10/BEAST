bb263ddff54d1f590de218ffb76a229fd7d1839d
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-04-18 14:15:43 +0530

    Update quick-embed.js

--- commands/MODERATION/quick-embed.js
+++ commands/MODERATION/quick-embed.js
@@ -11,11 +11,11 @@ module.exports = {
     if (!Tochannel) return message.channel.send('Specify a channel to send embed!')
     args.shift(); 
     const arg = args.join(" ");
-    const title = arg.split(',')[0];
+    const title = arg.split('^')[0];
     if (!title) return message.channel.send('Specify a title for the embed!')
-    const description = arg.split(',')[1];
+    const description = arg.split('^')[1];
     if (!description) return message.channel.send('Specify a description for the embed!')
-    const color = arg.split(',')[2];
+    const color = arg.split('^')[2];
     if (!footer) return message.channel.send('Specify a color for the embed!')
 
  

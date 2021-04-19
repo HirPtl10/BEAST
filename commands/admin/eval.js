ad06f14d13409d1aa83fb3a6d5f69c8fcaf5acfc
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-04-19 18:02:14 +0530

    Create eval.js

---
+++ commands/admin/eval.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js')
+
+const { inspect } = require('util')
+
+module.exports = {
+  name: 'eval',
+  run: async (client, message, args) => {
+  if (message.author.id !=== '827793921144913971') return;
+    const code = args.join(' ')
+    try {
+      const result = await eval(code);
+      let outpout = result;
+      if (typeof result !== 'string') {
+        output = inspect(result)
+      }
+      message.channel.send(output, {code: 'js'})
+    } catch (error) {
+      console.log(error)
+      message.channel.send(`Its too long`)
+    }
+  }
+}

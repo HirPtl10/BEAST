b4461f4652c014620061533059f7f86e77784108
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-04-19 12:42:02 +0530

    Update message.js

--- models/message.js
+++ models/message.js
@@ -1,7 +1,21 @@
-const mongoose = require('mongoose');
-const MessageSchema = new mongoose.Schema({
-    messageId: { type: String, required: true },
-    emojiRoleMappings: { type: mongoose.Schema.Types.Mixed }
-});
 
-const MessageModel = module.exports = mongoose.model('message', MessageSchema);
+const mongoose = require('mongoose')
+
+const reqString = {
+  type: String,
+  required: true,
+}
+
+const messageSchema = new mongoose.Schema({
+  guildId: reqString,
+  channelId: reqString,
+  messageId: reqString,
+  roles: [
+    {
+      emoji: reqString,
+      roleId: reqString,
+    },
+  ],
+})
+
+module.exports = mongoose.model('tutorial-message-schema', messageSchema)

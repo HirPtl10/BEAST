4490743ca6a42c34a83da6584728f5aea09a97cf
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-04-22 21:08:17 +0530

    Create loadSlashCommands.js

---
+++ handlers/loadSlashCommands.js
@@ -0,0 +1,13 @@
+
+const fs = require('fs');
+
+module.exports = async (client) => {
+	fs.readdir('./slashcommands', (err, files) => {
+		if(err) throw err;
+
+		files.forEach((file) => {
+			const slashcmd = require(`../slashcommands/${file}`);
+			client.slashcommands.set(slashcmd.config.name, slashcmd);
+		});
+	});
+};

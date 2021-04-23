1afaf327438f7e74b2056e3afe5d1585cf620203
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-04-23 16:56:35 +0530

    Update say.js

--- slashcommands/say.js
+++ slashcommands/say.js
@@ -14,7 +14,6 @@ module.exports = {
 	execute (client, interaction) {
 	const embed = new Discord.MessageEmbed()
 		.setDescription(args[0].value)
-		.setAuthor(`${interaction.member.user.username}`, new Discord.User(client, interaction.member.user).displayAvatarURL())
 		.setTimestamp()
 		.setColor('RANDOM');
 	client.api.interactions(interaction.id, interaction.token).callback.post({

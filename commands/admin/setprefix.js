44286e62ee3a528a3aaf5af9ca8c2da90b7dcc08
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-04-24 21:09:44 +0530

    Update setprefix.js

--- commands/admin/setprefix.js
+++ commands/admin/setprefix.js
@@ -9,7 +9,7 @@ module.exports = {
         prefixSchema.findOne({ Guild : message.guild.id }, async(err, data) => {
             if(err) throw err;
             if(data) {
-                prefixSchema.findOneAndDelete({ Guild : message.guild.id })
+                prefixSchema.findOneAndUpdate({ Guild : message.guild.id })
                 data = new prefixSchema({
                     Guild : message.guild.id,
                     Prefix : res
@@ -21,7 +21,7 @@ module.exports = {
                     Guild : message.guild.id,
                     Prefix : res
                 })
-                data.save()
+               
                 message.channel.send(`Custom prefix in this server is now set to **${res}**`)
             }
         })

41dc560975a63e4e2d719016f2d5649ced3c9005
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-04-24 20:49:54 +0530

    Update setprefix.js

--- commands/admin/setprefix.js
+++ commands/admin/setprefix.js
@@ -6,24 +6,26 @@ module.exports = {
     run : async(client, message, args) => {
         const res = await args.join(" ")
         if(!res) return message.channel.send('Please specify a prefix to change to.')
-        prefixSchema.findOne({ Guild : message.guild.id }, async(err, data) => {
-            if(err) throw err;
-            if(data) {
-                prefixSchema.findOneAndUpdate({ Guild : message.guild.id })
-                data = new prefixSchema({
-                    Guild : message.guild.id,
-                    Prefix : res
-                })
-                data.save()
-                message.channel.send(`Your prefix has been updated to **${res}**`)
-            } else {
-                data = new prefixSchema({
-                    Guild : message.guild.id,
-                    Prefix : res
-                })
-                data.save()
-                message.channel.send(`Custom prefix in this server is now set to **${res}**`)
-            }
+           let data;
+    try {
+        data = await PrefixSchema.findOne({
+            Guild: message.guild.id
         })
+        if(!data) {
+            let newdata = await PrefixSchema.create({
+                Guild: message.guild.id,
+                Prefix: res
+            })
+            newdata.save()
+        } else {
+            await PrefixSchema.findOneAndUpdate({
+                Guild: message.guild.id,
+                Prefix: newprefix,
+            })
+        }
+        message.channel.send(`The Prefix has Been set to ${red}`)
+    } catch (err) {
+        console.log(err)
+    }
     }
 }

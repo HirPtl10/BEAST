5ba38c98c36bfbe90ab0a6ed643095b0ffb2229d
PRO <hirangpatel2@gmail.com>
2021-04-27 21:04:45 +0530

    s

--- slashcommands/ping.js
+++ slashcommands/ping.js
@@ -6,12 +6,14 @@ module.exports = {
 	global: true,
 	execute (interaction, client) {
 		
-		client.api.interactions(interaction.id, interaction.token).callback.post({data: {
-			type: 4,
-			data: {
-					content: `Pong!`
-				}
-			}
+		client.api.interactions(interaction.id, interaction.token).callback.post({
+            data: {
+                type: 4,
+                data: {
+                    content: "hello world!!!"
+                }
+            }
 		})
-	},
-};
+	}
+}
+

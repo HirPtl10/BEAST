09123d1394714b526261c0243f5ae4c2e53b56c4
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-04-28 11:44:56 +0530

    Create nuke.js

---
+++ commands/MODERATION/nuke.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require('discord.js')
+module.exports = {
+    name: "nuke",
+    description: "Nukes a given channel",
+    usage: "nuke <rsn>",
+   
+    run: async(client, message, args) => {
+        if(!message.member.hasPermission("ADMINISTRATOR")) {
+            return message.reply("You do not have the perms to use this cmd! missing perms: `ADMINISTRATOR`")
+        }
+        let reason = args.join(" ") || "No Reason :/"
+        if(!message.channel.deletable) {
+            return message.reply("This channel cannot be nuked! what are you thinking?")
+        }
+        let newchannel = await message.channel.clone()
+        await message.channel.delete()
+        let embed = new MessageEmbed()
+        .setTitle(`BAM! \`${message.channel.name}\` **has been NUKED**`)
+        .setDescription(reason)
+        .setImage('https://cdn.discordapp.com/attachments/811143476522909718/819507596302090261/boom.gif')
+        await newchannel.send(embed)
+   

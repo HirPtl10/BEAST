76addbe4afc9d23574b67422c7a6e9e4822938b2
PRO <hirangpatel2@gmail.com>
2021-04-30 08:16:25 +0530

    a

--- test/slash.js
+++
@@ -1,8 +0,0 @@
-const client = require("../index");
-
-client.ws.on('INTERACTION_CREATE', async interaction => {
-    client.api.applications(client.user.id).commands.post({data: {
-        name: 'hey',
-        description: 'hello command'
-    }})
-})

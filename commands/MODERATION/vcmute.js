06ea93650a2472a485aebe5eeb42328354c2d17e
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-05-01 19:00:02 +0530

    Update vcmute.js

--- commands/MODERATION/vcmute.js
+++ commands/MODERATION/vcmute.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'vcmute',
   run: async (client, message, args) => {
     var member = message.mentions.members.first();
-    var reason = args.slice(1).join(' ')
-    member.setMute(reason)
+   
+    member.voice.setMute()
   }
 }

d5ce54ddcc3e67f2f372d77fa156ff6558816152
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-05-01 13:40:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,21 +22,7 @@ client.prefix = async function(message) {
         }
         return custom;
     }
-readdirSync("./events/").forEach((file) => {
-    const events = readdirSync("./events/").filter((file) =>
-      file.endsWith(".js")
-    );
 
-    for (let file of events) {
-      let pull = require(`../events/${file}`);
-
-      if (pull.name) {
-        client.events.set(pull.name, pull);
-      } else {
-        continue;
-      }
-    }
-  });
 
 module.exports = client;
 

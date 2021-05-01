4331abdf8f70e8335fc449bef7594eaddbf40298
PRO <hirangpatel2@gmail.com>
2021-05-01 16:20:47 +0530

    a

--- index.js
+++ index.js
@@ -23,7 +23,21 @@ client.prefix = async function(message) {
         return custom;
     }
 
-
+    readdirSync("./events/").forEach((file) => {
+      const events = readdirSync("./events/").filter((file) =>
+        file.endsWith(".js")
+      );
+  
+      for (let file of events) {
+        let pull = require(`../events/${file}`);
+  
+        if (pull.name) {
+          client.events.set(pull.name, pull);
+        } else {
+          continue;
+        }
+      }
+    });
 module.exports = client;
 
 const mongoose = require('mongoose');

8769b5dfa0b28f5a6372a5bf4e403a72f247352f
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-05-01 20:39:14 +0530

    Create panel.js

---
+++ commands/test/panel.js
@@ -0,0 +1,29 @@
+const { Client, Message, MessageEmbed, Util } = require('discord.js')
+const Schema = require('../../models/reaction-roles')
+module.exports = {
+    name: 'panel',
+
+
+    run: async (client, message, args) => {
+        if (!message.member.permissions.has('ADMINISTRATOR')) return;
+
+        const channel = message.mentions.channels.first() || message.channel;
+
+        Schema.findOne({ Guild: message.guild.id }, async (err, data) => {
+            if (!data) return;
+            const mapped = Object.keys(data.Roles)
+                .map((value, index) => {
+                    const role = message.guild.roles.cache.get(data.Roles[value][0]);
+                    return `${index + 1}) ${data.Roles[value][1].raw} - ${role}`
+                }).join('\n\n');
+
+                channel.send(new MessageEmbed().setDescription(mapped)).then((msg) => {
+                    data.Message = msg.id;
+                    data.save();
+
+                    const reactions = Object.values(data.Roles).map((val) => val[1].id);
+                    reactions.map((emoji) => msg.react(emoji));
+                });
+        });
+    }
+}

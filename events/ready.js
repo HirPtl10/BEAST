b829a5a22b819830e3938c601ddaa74b8ff66de1
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-05-02 18:35:40 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,20 +1,6 @@
-const client = require('../index')
-const mongoose = require('mongoose')
-const app = require('express')
-const model = mongoose.model("Money", {})
-client.on('ready', () => {
-    console.log('ONLINE')
-client.user.setActivity(`Does it better`, { type: 'WATCHING' })
-    app.length("/economy", (req, res) => {
-        const user = req.query.user;
-        if(!user)
-        {
-            model.find({}, (err, data) => res.send(data))
-                if(!data) return res.send('This user has no data')
-                res.send(data)
-        }
-        
+const Discord = require("discord.js")
 
-    })
-    app.listen(9154)
-})
+module.exports = client => { 
+    console.log(`${client.user.username} is online`)
+    client.user.setActivity("Wut", {type: "PLAYING"});
+}

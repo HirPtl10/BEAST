831057fc407d197b14e40981fec54e51e18eb716
PRO <hirangpatel2@gmail.com>
2021-05-03 13:33:24 +0530

    a

--- commands/test/panel.js
+++ commands/test/panel.js
@@ -1,65 +1,28 @@
-const Discord = require('discord.js');
-const Schema = require('../../models/reaction-roles');
-
+const { Client, Message, MessageEmbed, Util } = require('discord.js')
+const Schema = require('../../models/reaction-roles')
 module.exports = {
-  name: 'panel',
- 
-  run: async (client, message, args) => {
-    const noPermissionEmbed = new Discord.MessageEmbed()
-      .setColor('#f54842')
-      .setTitle('❗ INVALID PERMISSION')
-      .setDescription(`You don't have that permission !`)
-      .addField('Permission Required', '`ADMINISTRATOR`')
-      .setFooter(client.user.username, client.user.displayAvatarURL())
-      .setTimestamp();
+    name: 'panel',
 
-    const clientNoPermissionEmbed = new Discord.MessageEmbed()
-      .setColor('#f54842')
-      .setTitle('❗ INVALID PERMISSION')
-      .setDescription(`The client doesn't have that permission !`)
-      .addField('Permission Required', 'ADMINISTRATOR')
-      .setFooter(client.user.username, client.user.displayAvatarURL())
-      .setTimestamp();
+    run: async (client, message, args) => {
+        if (!message.member.permissions.has('ADMINISTRATOR')) return;
 
-    if (!message.member.hasPermission('ADMINISTRATOR'))
-      return message.channel.send(noPermissionEmbed);
-    if (!message.guild.me.hasPermission('ADMINISTRATOR'))
-      return message.channel.send(clientNoPermissionEmbed);
+        const channel = message.mentions.channels.first() || message.channel;
 
-    const channel = message.mentions.channels.first() || message.channel;
+        Schema.findOne({ Guild: message.guild.id }, async (err, data) => {
+            if (!data) return;
+            const mapped = Object.keys(data.Roles)
+                .map((value, index) => {
+                    const role = message.guild.roles.cache.get(data.Roles[value][0]);
+                    return `${index + 1}) ${data.Roles[value][1].raw} - ${role}`
+                }).join('\n\n');
 
-    Schema.findOne({ Guild: message.guild.id }, async (err, data) => {
-      const noDataEmbed = new Discord.MessageEmbed()
-        .setColor('#f54842')
-        .setTitle('❗ ERROR')
-        .setDescription(`No data saved in this server !`)
-        .setFooter(client.user.username, client.user.displayAvatarURL())
-        .setTimestamp();
-      if (!data) return message.channel.send(noDataEmbed);
+                channel.send(new MessageEmbed().setDescription(mapped)).then((msg) => {
+                    data.Message = msg.id;
+                    data.save();
 
-      const mapped = Object.keys(data.Roles)
-        .map((value, index) => {
-          const role = message.guild.roles.cache.get(data.Roles[value][0]);
-          return `${index + 1} ${data.Roles[value][1].raw} - ${role}`;
-        })
-        .join('\n\n');
-
-      const rolesPanelEmbed = new Discord.MessageEmbed()
-        .setColor('#0099ff')
-        .setTitle('Reaction Roles Panel')
-        .setDescription(mapped)
-        .setFooter(client.user.username, client.user.displayAvatarURL())
-        .setTimestamp();
-
-      channel.send(rolesPanelEmbed).then((msg) => {
-        data.Message = msg.id;
-        data.save();
-
-        const reactions = Object.values(data.Roles).map(
-          (val) => val[1].id
-        );
-        reactions.map((emoji) => msg.react(emoji))
-      });
-    });
-  },
-};
+                    const reactions = Object.values(data.Roles).map((val) => val[1].id);
+                    reactions.map((emoji) => msg.react(emoji));
+                });
+        });
+    }
+}

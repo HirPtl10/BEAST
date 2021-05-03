2d4b68405659e89e090e28c0bc73d7008c9c08ac
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-05-03 22:20:53 +0530

    Update discrim.js

--- commands/UTIL/discrim.js
+++ commands/UTIL/discrim.js
@@ -6,13 +6,14 @@ const discrim = args[0] ?? message.author.discriminator;
         if (isNaN(discrim) || discrim.length !== 4) return message.reply("❌ | Invalid discriminator");
 
         const embed = new MessageEmbed()
-            .setTitle(`Users with discriminator #${discrim}`)
-            .setDescription(this.client.users.cache.filter(u => u.discriminator == discrim).array().slice(0, 10).map((m, i) => `${i+1}. ${m.tag}`).join("\n"))
-           
-            .setTimestamp()
-            .setColor("RANDOM");
 
-        return message.reply(embed);
+            embed.setTitle(`Users with discriminator #${discrim}`)
+            embed.setDescription(this.client.users.cache.filter(u => u.discriminator == discrim).array().slice(0, 10).map((m, i) => `${i+1}. ${m.tag}`).join("\n"))
+           
+            embed.setTimestamp()
+            embed.setColor("RANDOM");
+
+        return message.channel.send(embed);
     }
 
 }

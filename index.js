915dbe75fbade96ed3844411523e3096c2b0c428
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-05-05 21:36:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,24 +31,7 @@ client.prefix = async function(message) {
     }
 client.on('ready', () => {
  console.log(`${client.user.username} is online`)
-    client.user.setActivity("Wut", {type: "PLAYING"});
-
-    const clientDetails = {
-        guilds: client.guilds.cache.size,
-        users: client.users.cache.size
-    }
-    const express = require('express')
-    const app = express();
-    const port = process.env.PORT || 5001
-	app.set("view engine", "ejs")
-    app.get("/", (req, res) => {
-        res.status(200).sendFile(path.join(__dirname, "..", "pages", "landingPage.html"))
-    })
-    app.get("/info", (req, res) => {
-        res.status(200).send(clientDetails)
-    })
-    app.listen(port)
-   
+ client.user.setActivity("Wut", {type: "PLAYING"});
 });
   
 

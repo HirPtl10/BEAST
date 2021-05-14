8ac242a85943a3ac53dca96c9a1967fe595321bd
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-05-14 22:24:51 +0530

    Update hackban.js

--- commands/MODERATION/hackban.js
+++ commands/MODERATION/hackban.js
@@ -3,6 +3,14 @@ module.exports = {
   name: 'hackban',
   description: 'bans member outside server',
   run: async (client, message, args) => {
+	   const answers = [
+		'https://tenor.com/view/cosmic-ban-hammers-gif-20478007',
+		'https://tenor.com/view/star-wars-banhammer-moderator-ban-discord-gif-17302394',
+		'https://tenor.com/view/thor-banhammer-discord-banned-banned-by-admin-gif-12646581',
+		'https://tenor.com/view/discord-ban-server-discord-server-discord-server-ban-gif-18150385',
+		'https://tenor.com/view/banned-admin-hulk-gif-18033317',
+		
+	];
     if(!message.member.permissions.has("BAN_MEMBERS")) return;
     if(!message.guild.me.permissions.has("BAN_MEMBERS")) return message.reply(`I Do not have permission to ban members`);
   const useri = args[0];
@@ -15,7 +23,7 @@ module.exports = {
     if (msgs.first().content.toLowerCase() === 'yes') {
     client.users.fetch(useri).then(async user => {
       await message.guild.members.ban(useri, {reason: reason});
-      return message.channel.send(`https://tenor.com/view/discord-ban-server-discord-server-discord-server-ban-gif-18150385\n<@${useri}> has been banned from the server`);
+      return message.channel.send(`${answers[Math.floor(Math.random() * answers.length)]} \n <@${useri}> has been banned from the server`);
     })
       } else {
  return message.reply(`Cancelles`)

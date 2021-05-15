8933f1cc58453bd6bffa2133cc85e268482c4643
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-05-15 11:03:32 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,23 @@
 const client = require('../index')
 
 client.on('ready', () => {
-  console.log(`Bot is Online`)
-  client.user.setActivity(`Developed Using DJS`, { type: 'PLAYING' })
-})
+ console.log(`Bot is online`)
+    client.user.setActivity("Developer Using DJS", {type: "PLAYING"});
+
+    const clientDetails = {
+        guilds: client.guilds.cache.size,
+        users: client.users.cache.size
+    }
+    const express = require('express')
+    const app = express();
+    const port = process.env.PORT || 5001
+    app.set("view engine", "ejs")
+    app.get("/", (req, res) => {
+        res.status(200).sendFile(path.join(__dirname, "..", "pages", "landingPage.html"))
+    })
+    app.get("/info", (req, res) => {
+        res.status(200).send(clientDetails)
+    })
+    app.listen(port)
+   
+});

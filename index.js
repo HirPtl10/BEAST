da7b0c946f17732b78fe6b592e597e9929a85c16
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-06-18 22:50:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -135,12 +135,6 @@ client.on('guildDelete', async (guild) => {
 })      
 
 
-const distube = require('distube')
-const player = new distube(client)
-player.on('playSong', (message, queue, song) => {
-	message.channel.send(`${song.name} Started playing ok?`)
-})
-client.player = player; 
 
 client.login(process.env.token)
 

63e51bf01f2a55b7f91973bcd38521805a0eba00
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-06-18 16:54:16 +0530

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -2,9 +2,9 @@ module.exports = {
     name: 'play',
 run: async (client, message, args) => {
 
-    if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
+    if (!message.member.voice.channel) return message.channel.send(`You're not in a voice channel !`);
 
-    if (!args[0]) return message.channel.send(`${client.emotes.error} - Please indicate the title of a song !`);
+    if (!args[0]) return message.channel.send(`Please indicate the title of a song !`);
 
     client.player.play(message, args.join(" "));
 

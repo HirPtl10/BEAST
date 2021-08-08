bff8c6e1fd1e5c8f104efc056f23a2082ec6bfa8
Your Name <you@example.com>
2021-08-08 11:18:39 +0530

    a

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ mongoose.connect('mongodb+srv://last:last@test.8ukwy.mongodb.net/Data', { useNew
       
 
 
-//
+//ffffffff
 client.bal = (id) => new Promise(async  ful => {
     const data = await schema.findOne({ id })
     if (!data) return ful(0);

c9965e0572a8d36f8a9941de602d4a033d57fb05
PRO <75833882+PRO6969OP@users.noreply.github.com>
2021-08-09 20:05:58 +0530

    Update command.js

--- handlers/command.js
+++ handlers/command.js
@@ -1,36 +1,49 @@
+const { glob } = require("glob");
+const { promisify } = require("util");
+const { Client } = require("discord.js");
 
-const {readdirSync} = require('fs');
-const ascii = require('ascii-table')
-let table = new ascii("Commands");
-table.setHeading('Command', ' Load status');
-module.exports= (client) => {
-    readdirSync('./commands/').forEach(dir => {
-        const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith('.js'));
-        for(let file of commands){
-            let pull = require(`../commands/${dir}/${file}`);
-            if(pull.name){
-                client.commands.set(pull.name, pull);
-                table.addRow(file,'✅')
-            } else {
-                table.addRow(file, '❌ -> Missing a help.name, or help.name is not a string.')
-                continue;
-            }if(pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name))
+const globPromise = promisify(glob);
+
+/**
+ * @param {Client} client
+ */
+module.exports = async (client) => {
+    // Commands
+    const commandFiles = await globPromise(`${process.cwd()}/commands/**/*.js`);
+    commandFiles.map((value) => {
+        const file = require(value);
+        const splitted = value.split("/");
+        const directory = splitted[splitted.length - 2];
+
+        if (file.name) {
+            const properties = { directory, ...file };
+            client.commands.set(file.name, properties);
         }
     });
-    console.log(table.toString());
-    readdirSync("./events/").forEach((file) => {
-    const events = readdirSync("./events/").filter((file) =>
-      file.endsWith(".js")
+
+    // Events
+    const eventFiles = await globPromise(`${process.cwd()}/events/*.js`);
+    eventFiles.map((value) => require(value));
+
+    // Slash Commands
+    const slashCommands = await globPromise(
+        `${process.cwd()}/SlashCommands/*/*.js`
     );
 
-    for (let file of events) {
-      let pull = require(`../events/${file}`);
+    const arrayOfSlashCommands = [];
+    slashCommands.map((value) => {
+        const file = require(value);
+        if (!file?.name) return;
+        client.slashCommands.set(file.name, file);
+        arrayOfSlashCommands.push(file);
+    });
+    client.on("ready", async () => {
+        // Register for a single guild
+        await client.guilds.cache
+            .get("replace this with your guild id")
+            .commands.set(arrayOfSlashCommands);
 
-      if (pull.name) {
-        client.events.set(pull.name, pull);
-      } else {
-        continue;
-      }
-    }
-  });
-}
+        // Register for all the guilds the bot is in
+        // await client.application.commands.set(arrayOfSlashCommands);
+    });
+};
